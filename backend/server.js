cddf435f458dd2f1091c75eda46597be4ce8f501
Henry Zx <henryraghav007@gmail.com>
2025-05-29 18:36:33 +0530

    Fix: invalid route parameter in route files

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-import orderRoutes from "./routes/order.route.js"; // <-- Added
 
 import { connectDB } from "./lib/db.js";
 
@@ -20,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -29,17 +28,16 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
-app.use("/api/orders", orderRoutes); // <-- Added
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
-  connectDB();
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
 });
